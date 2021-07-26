8e774b2cf675d04906bc734967730de4d538b9b0
Tarun <tarun010@zohomail.in>
2021-07-27 00:13:40 +0530

    added github api

--- app.py
+++ app.py
@@ -1,21 +1,49 @@
 from flask import Flask, render_template
+import requests
+
+
+user_name = "Tarun"
+
+github_projects_url = "https://api.github.com/users/tarun010/repos"
+projects_from_github = requests.get(github_projects_url).json()
+
+projects = []
+
+for project in projects_from_github:
+    name = project["name"]
+    desc = project["description"]
+    url = project["html_url"]
+
+    projects.append({
+        "name": name,
+        "desc": desc,
+        "url": url
+    })
+
+print(projects)
+
+
+
+list_of_food = [
+    "Tea",
+    "Apples",
+    "Ramen"
+]
 
 app = Flask(__name__)
 
-name = "Tarun"
-
 @app.route("/")
 def about_page():
-    return render_template("about.html", name=name)
+    return render_template("about.html", name=user_name, list_of_food=list_of_food)
 
 @app.route("/contact")
 def contact_page():
-    return render_template("contact.html", name=name)
+    return render_template("contact.html", name=user_name)
 
 @app.route("/projects")
 def project_page():
-    return render_template("projects.html", name=name)
+    return render_template("projects.html", name=user_name, projects=projects)
 
 @app.route("/blog")
 def blog_page():
-    return render_template("blog.html", name=name)
+    return render_template("blog.html", name=user_name)
