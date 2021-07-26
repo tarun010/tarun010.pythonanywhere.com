3c95ef461d4e8f2a136b49120d67e7e41a308fdf
Tarun <tarun010@zohomail.in>
2021-07-26 19:31:00 +0530

    upaded app.py

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.route("/")
+name = "Tarun"
+
+@app.route("/")
 def about_page():
-    
+    return render_template("about.html", name=name)
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html", name=name)
+
+@app.route("/projects")
+def project_page():
+    return render_template("projects.html", name=name)
+
+@app.route("/blog")
+def blog_page():
+    return render_template("blog.html", name=name)
